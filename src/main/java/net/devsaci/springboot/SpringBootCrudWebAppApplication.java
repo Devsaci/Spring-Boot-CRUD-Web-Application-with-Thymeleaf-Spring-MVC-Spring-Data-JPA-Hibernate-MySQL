772cf26fd4e778405cc5a0ceb56c0adbc397a95d
saci zakaria <sacideveloppeur@gmail.com>
2020-06-15 21:39:54 +0200

    Step 1: Create and Setup Spring Boot Project in Eclipse STS IDE

---
+++ src/main/java/net/devsaci/springboot/SpringBootCrudWebAppApplication.java
@@ -0,0 +1,13 @@
+package net.devsaci.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudWebAppApplication.class, args);
+	}
+
+}
